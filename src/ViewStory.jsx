1ce5caf65d2603017d84a2e7a989bd641c35f873
MANIKANDAN.K <115163665+Its-manikandan@users.noreply.github.com>
2026-02-10 11:31:07 +0530

    Update ViewStory.jsx

--- src/ViewStory.jsx
+++ src/ViewStory.jsx
@@ -1,96 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import { useParams , Link , useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
 function ViewStory() {
-    const { id,tot } = useParams();
+  const { id, tot } = useParams();
+  const navigate = useNavigate();
 
-    const [story, setstory] = useState(null);
+  const currentId = Number(id);
+  const total = Number(tot);
 
-    const navigate = useNavigate();
+  const [story, setStory] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch(`https://json-backend-sntc.onrender.com/story/${id}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log("Story Data:",data);
-                setstory(data)})
-            .catch(err => console.log(err));
-    }, [id]);
 
-    if (id > tot || id<=0){
-        navigate("/");
+  useEffect(() => {
+    if (currentId <= 0 || currentId > total) {
+      navigate("/");
     }
-    return (
-        <div>
-            {story? (
-                <div className="d-flex justify-content-center align-items-center">
-                    <Link to={`https://json-backend-sntc.onrender.com/story/${Number(id)-1}/${tot}`}><i class="bi bi-arrow-left-circle-fill"></i></Link>
-                    <img className="vh-100" src={story.image} alt="story" />
-                    <Link to={`https://json-backend-sntc.onrender.com/story/${Number(id)+1}/${tot}`}><i class="bi bi-arrow-right-circle-fill"></i></Link>
-                </div>
-            ) : (
-                <div>Loading</div>
-            )}
-        </div>
-    );
+  }, [currentId, total, navigate]);
+
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(
+      `https://json-backend-sntc.onrender.com/story?id=${currentId}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setStory(data[0]); 
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.error("Error fetching story:", err);
+        navigate("/");
+      })
+      .finally(() => setLoading(false));
+  }, [currentId, navigate]);
+
+  if (loading) {
+    return <div className="text-center mt-5">Loading...</div>;
+  }
+
+  if (!story) {
+    return null;
+  }
+
+  
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      {}
+      <Link
+        to={`/story/${currentId - 1}/${total}`}
+        className="mx-3"
+      >
+        <i className="bi bi-arrow-left-circle-fill fs-2"></i>
+      </Link>
+
+      {}
+      <img
+        src={story.image}
+        alt="story"
+        className="vh-100"
+        style={{ objectFit: "contain" }}
+      />
+
+      {}
+      <Link
+        to={`/story/${currentId + 1}/${total}`}
+        className="mx-3"
+      >
+        <i className="bi bi-arrow-right-circle-fill fs-2"></i>
+      </Link>
+    </div>
+  );
 }
 
-export default ViewStory
-
-// const { id, tot } = useParams();
-
-// const currentId = Number(id);
-// const total = Number(tot);
-// useEffect(() => {
-//   if (currentId > total) {
-//     navigate("/");
-//   }
-// }, [currentId, total, navigate]);
-// const goNext = () => {
-//   if (currentId === total) {
-//     navigate("/"); // go home on last story
-//   } else {
-//     navigate(`/story/${currentId + 1}/${total}`);
-//   }
-// };
-
-// const goPrev = () => {
-//   if (currentId > 1) {
-//     navigate(`/story/${currentId - 1}/${total}`);
-//   }
-// };
-// return (
-//   <div>
-//     {story ? (
-//       <div className="d-flex justify-content-center align-items-center">
-
-//         {/* PREVIOUS */}
-//         {currentId > 1 && (
-//           <i
-//             className="bi bi-arrow-left-circle-fill fs-2 me-3"
-//             style={{ cursor: "pointer" }}
-//             onClick={goPrev}
-//           ></i>
-//         )}
-
-//         {/* STORY IMAGE */}
-//         <img
-//           src={story.image}
-//           alt="story"
-//           className="vh-100"
-//         />
-
-//         {/* NEXT */}
-//         <i
-//           className="bi bi-arrow-right-circle-fill fs-2 ms-3"
-//           style={{ cursor: "pointer" }}
-//           onClick={goNext}
-//         ></i>
-
-//       </div>
-//     ) : (
-//       <div>Loading...</div>
-//     )}
-//   </div>
-// );
+export default ViewStory;
 
